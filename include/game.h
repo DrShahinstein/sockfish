9f19b5d422879845526b162d4f1b9d24a3f641e5
DrShahinstein <1taylanozturk@protonmail.com>
2025-08-06 23:41:19 +0300

    Create struct Drag to handle drag & drop

--- include/game.h
+++ include/game.h
@@ -2,10 +2,19 @@
 
 #include <SDL3/SDL.h>
 
+typedef struct {
+  int row;
+  int col;
+  int from_row;
+  int from_col;
+  bool active;
+} Drag;
+
 struct GameState {
   bool running;
   char board[8][8];
   SDL_Texture *tex[128];
+  Drag drag;
 };
 
 void draw_game(SDL_Renderer *renderer, struct GameState *state);
