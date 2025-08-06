505d71a32d019133ce754ecbd704e31dc98220bf
DrShahinstein <1taylanozturk@protonmail.com>
2025-08-06 17:39:46 +0300

    Update board.h for new functions

--- include/board.h
+++ include/board.h
@@ -3,5 +3,10 @@
 #include "game.h"
 
 #define SQ 100
+#define PIECE_PATH "assets/pieces/%c.png"
 
+void load_fen(const char *fen, struct GameState *state);
+void load_piece_textures(SDL_Renderer *renderer, struct GameState *state);
+void initialize_board(SDL_Renderer *renderer, struct GameState *state);
+void cleanup_textures(struct GameState *state);
 void draw_board(SDL_Renderer *renderer, struct GameState *state);
