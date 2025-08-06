0edc9dd7d28f7fbe4afdd479f027c2ce5864a698
DrShahinstein <1taylanozturk@protonmail.com>
2025-08-06 17:41:36 +0300

    Call draw_board() properly

--- src/game.c
+++ src/game.c
@@ -3,5 +3,8 @@
 #include <SDL3/SDL.h>
 
 void draw_game(SDL_Renderer *renderer, struct GameState *state) {
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+  SDL_RenderClear(renderer);
   draw_board(renderer, state);
+  SDL_RenderPresent(renderer);
 }
