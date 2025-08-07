ac0d85b197fcdb6f4618110fa53420fc2489c0f9
DrShahinstein <1taylanozturk@protonmail.com>
2025-08-07 20:59:52 +0300

    Integrate ui

--- src/main.c
+++ src/main.c
@@ -2,15 +2,15 @@
 #include "game.h"
 #include "board.h"
 #include "event.h"
+#include "ui.h"
 #include <SDL3/SDL.h>
 
 int main(int argc, char *argv[]) {
-  (void)argc;
-  (void)argv;
+  (void)argc; (void)argv;
 
   SDL_Init(SDL_INIT_VIDEO);
 
-  SDL_Window *window = SDL_CreateWindow(W_TITLE, W_SIZE, W_SIZE, 0);
+  SDL_Window *window = SDL_CreateWindow(W_TITLE, W_WIDTH, W_HEIGHT, 0);
   if (window == NULL) {
     SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
     SDL_Quit();
@@ -23,20 +23,25 @@ int main(int argc, char *argv[]) {
     return 1;
   }
   
+  UI_State ui;
   struct GameState state = {
     .running = true,
   };
 
   initialize_board(renderer, &state);
+  ui_init(&ui);
 
   while (state.running) {
     SDL_Event e;
 
     while (SDL_PollEvent(&e)) {
       handle_event(&e, &state);
+      ui_handle_event(&ui, &e);
     }
     
     draw_game(renderer, &state);
+    ui_draw(renderer, &ui);
+    SDL_RenderPresent(renderer);
   }
 
   cleanup_textures(&state);
