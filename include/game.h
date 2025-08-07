3ece00d2bb608a635762344d692bd89fe5ef03cd
DrShahinstein <1taylanozturk@protonmail.com>
2025-08-07 18:06:52 +0300

    Create Drag structure nested in GameState

--- include/game.h
+++ include/game.h
@@ -2,19 +2,17 @@
 
 #include <SDL3/SDL.h>
 
-typedef struct {
-  int row;
-  int col;
-  int from_row;
-  int from_col;
-  bool active;
-} Drag;
-
 struct GameState {
   bool running;
   char board[8][8];
   SDL_Texture *tex[128];
-  Drag drag;
+  struct {
+    int row;
+    int col;
+    int from_row;
+    int from_col;
+    bool active;
+  } drag;
 };
 
 void draw_game(SDL_Renderer *renderer, struct GameState *state);
