d6e2d28dc1207e952aeca4dae92d7569ba968626
DrShahinstein <1taylanozturk@protonmail.com>
2025-08-07 21:00:20 +0300

    Remove RenderPresent() from draw_game()

--- src/game.c
+++ src/game.c
@@ -6,5 +6,4 @@ void draw_game(SDL_Renderer *renderer, struct GameState *state) {
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
   SDL_RenderClear(renderer);
   draw_board(renderer, state);
-  SDL_RenderPresent(renderer);
 }
