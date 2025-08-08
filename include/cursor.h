dc5f292fd0a78e93764dc7d9518a366b6a4834d6
DrShahinstein <1taylanozturk@protonmail.com>
2025-08-08 18:12:52 +0300

    Make cursor.h definitions sensitively

--- include/cursor.h
+++ include/cursor.h
@@ -3,10 +3,12 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_mouse.h>
 
+#ifndef CURSOR_DEFAULT
 #define CURSOR_DEFAULT SDL_CreateSystemCursor(SDL_SYSTEM_CURSOR_DEFAULT)
-#define CURSOR_POINTER SDL_CreateSystemCursor(SDL_SYSTEM_CURSOR_POINTER)
+#endif
 
-void cleanup_cursors() {
-  if (CURSOR_POINTER) SDL_DestroyCursor(CURSOR_POINTER);
-  if (CURSOR_DEFAULT) SDL_DestroyCursor(CURSOR_DEFAULT);
-}
+#ifndef CURSOR_POINTER
+#define CURSOR_POINTER SDL_CreateSystemCursor(SDL_SYSTEM_CURSOR_POINTER)
+#endif
+
+void cleanup_cursors();
