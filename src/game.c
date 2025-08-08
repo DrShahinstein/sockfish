60d98c0113fc0e181f25b1be4c62f03ea7be7fdd
DrShahinstein <1taylanozturk@protonmail.com>
2025-08-08 12:07:11 +0300

    Draw ui in draw_game() function

--- src/game.c
+++ src/game.c
@@ -1,9 +1,10 @@
 #include "game.h"
 #include "board.h"
+#include "ui.h"
 #include <SDL3/SDL.h>
 
-void draw_game(SDL_Renderer *renderer, struct GameState *state) {
-  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-  SDL_RenderClear(renderer);
-  draw_board(renderer, state);
+void draw_game(SDL_Renderer *renderer, struct GameState *game_state, UI_State *ui_state) {
+  draw_board(renderer, game_state);
+  ui_draw(renderer, ui_state);
+  SDL_RenderPresent(renderer);
 }
