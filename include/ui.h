46c098998bd0416b1948a6b7c3f9d11c5896d8a8
DrShahinstein <1taylanozturk@protonmail.com>
2025-08-08 17:34:26 +0300

    Make better structs and add definitons

--- include/ui.h
+++ include/ui.h
@@ -1,23 +1,25 @@
 #pragma once
 
 #include <SDL3/SDL.h>
+#include <SDL3_ttf/SDL_ttf.h>
 
 #define UI_WIDTH 250
+#define UI_PADDING 10
+#define ROBOTO "assets/Roboto-Regular.ttf"
 
 typedef struct {
   SDL_FRect rect;
   bool hovered;
   bool active;
-} UI_Widget;
+} UI_Button;
 
 typedef struct {
-  UI_Widget reset_button;
-  UI_Widget loadfen_button;
-  SDL_FRect fen_box;
-  char fen_buffer[128];
   bool engine_on;
+  UI_Button toggle_button;
+  TTF_Font *font;
 } UI_State;
 
 void ui_init(UI_State *ui);
 void ui_handle_event(UI_State *ui, SDL_Event *e);
 void ui_draw(SDL_Renderer *renderer, UI_State *ui);
+void ui_destroy(UI_State *ui);
