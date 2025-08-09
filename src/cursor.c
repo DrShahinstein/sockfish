47b937d79690a58e5b0e5ac3e82327d88d9ba216
DrShahinstein <1taylanozturk@protonmail.com>
2025-08-09 12:04:41 +0300

    Implement cursor_in_rect() func in cursor.c

--- src/cursor.c
+++ src/cursor.c
@@ -12,3 +12,8 @@ void cleanup_cursors(void) {
   if (cursor_pointer) SDL_DestroyCursor(cursor_pointer);
   if (cursor_default) SDL_DestroyCursor(cursor_default);
 }
+
+bool cursor_in_rect(float x, float y, SDL_FRect *r) {
+  return x >= r->x && x < (r->x + r->w) && y >= r->y && y < (r->y + r->h);
+}
+
