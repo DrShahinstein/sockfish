7c18874be7af0a3050a0af8565a2f65044bec2d7
DrShahinstein <1taylanozturk@protonmail.com>
2025-08-09 14:26:35 +0300

    Organize definitions of ui types

--- include/ui.h
+++ include/ui.h
@@ -6,16 +6,25 @@
 #define UI_WIDTH 250
 #define UI_PADDING 10
 #define ROBOTO "assets/Roboto-Regular.ttf"
+#define MAX_FEN 128
 
 typedef struct {
   SDL_FRect rect;
   bool hovered;
   bool active;
-} UI_Button;
+} UI_Element;
+
+typedef struct {
+  UI_Element area;
+  UI_Element btn;
+  char input[MAX_FEN];
+  size_t length;
+} UI_FenLoader;
 
 typedef struct {
   bool engine_on;
-  UI_Button toggle_button;
+  UI_Element toggle_btn;
+  UI_FenLoader fen_loader;
   TTF_Font *font;
 } UI_State;
 
