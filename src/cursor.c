e9d77c264d46a86c0a845c3a2c0b14476fbea207
DrShahinstein <1taylanozturk@protonmail.com>
2025-08-09 17:42:54 +0300

    Implement cursor_text in cursor.c

--- src/cursor.c
+++ src/cursor.c
@@ -2,15 +2,18 @@
 
 SDL_Cursor *cursor_default = NULL;
 SDL_Cursor *cursor_pointer = NULL;
+SDL_Cursor *cursor_text    = NULL;
 
 void init_cursors(void) {
   cursor_default = SDL_CreateSystemCursor(SDL_SYSTEM_CURSOR_DEFAULT);
   cursor_pointer = SDL_CreateSystemCursor(SDL_SYSTEM_CURSOR_POINTER);
+  cursor_text    = SDL_CreateSystemCursor(SDL_SYSTEM_CURSOR_TEXT);
 }
 
 void cleanup_cursors(void) {
   if (cursor_pointer) SDL_DestroyCursor(cursor_pointer);
   if (cursor_default) SDL_DestroyCursor(cursor_default);
+  if (cursor_text)    SDL_DestroyCursor(cursor_text);
 }
 
 bool cursor_in_rect(float x, float y, SDL_FRect *r) {
