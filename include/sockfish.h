2cc0c81aaa940b7fdffcb05eb9b621093b7afd94
DrShahinstein <1taylanozturk@protonmail.com>
2025-08-11 17:27:13 +0300

    Reset sockfish base code

--- include/sockfish.h
+++ include/sockfish.h
@@ -5,7 +5,6 @@
 typedef struct GameState GameState;
 typedef enum { WHITE, BLACK } Turn;
 typedef struct Sockfish Sockfish;
-typedef struct SF_Move SF_Move;
 
 Sockfish *sf_create();
 void sf_destroy(Sockfish *sf);
