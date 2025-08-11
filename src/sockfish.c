01df7e247e1f77dacfdeb66a6e34c526a0bc5748
DrShahinstein <1taylanozturk@protonmail.com>
2025-08-11 15:39:37 +0300

    Lightly arrange initialization & lifecycle for Sockfish

--- src/sockfish.c
+++ src/sockfish.c
@@ -1,8 +1,38 @@
 #include "sockfish.h"
+#include "stdlib.h"
+#include "ui.h" // MAX_FEN
 
-void sf_init(GameState *game){
-  (void)(game);
+
+struct SF_Move {
+
+};
+
+struct Sockfish {
+  bool engine_on;
+  Turn search_color;
+  SDL_Thread *thread;
+  SDL_AtomicInt searching;
+  SDL_AtomicInt stop_req;
+  SDL_Mutex *lock;
+  char fen[MAX_FEN];
+  SF_Move best_move;
+};
+
+Sockfish *sf_create() {
+  Sockfish *sf = (Sockfish *)malloc(sizeof(Sockfish));
+  if (!sf) return NULL;
+  sf->thread = NULL;
+  sf->lock = SDL_CreateMutex();
+  SDL_SetAtomicInt(&sf->searching, 0);
+  SDL_SetAtomicInt(&sf->stop_req, 0);
+  sf->fen[0] = '\0';
+  sf->search_color = WHITE;
+  return sf;
 }
-void sf_destroy(GameState *game){
-  (void)(game);
+
+void sf_destroy(Sockfish *sf) {
+  if (!sf) return;
+  // sf_stop_search(sf);
+  if (sf->lock) SDL_DestroyMutex(sf->lock);
+  free(sf);
 }
