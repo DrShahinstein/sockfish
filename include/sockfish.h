a68b2731ac37392db8aa4ffa6b5c53e738bbd7fb
DrShahinstein <1taylanozturk@protonmail.com>
2025-08-15 19:31:22 +0300

    Maintain sockfish.h

--- include/sockfish.h
+++ include/sockfish.h
@@ -1,11 +1,32 @@
 #pragma once
 
+#if defined(_WIN32) || defined(__CYGWIN__)
+#  ifndef SDL_BeginThreadFunction
+#    define SDL_BeginThreadFunction NULL
+#  endif
+#  ifndef SDL_EndThreadFunction
+#    define SDL_EndThreadFunction NULL
+#  endif
+#endif
+
 #include <SDL3/SDL.h>
 
 typedef enum { WHITE, BLACK } Turn;
+
+typedef struct Move {
+  int fr; int fc;
+  int tr; int tc;
+} Move;
+
 typedef struct Sockfish {
+  SDL_Mutex *mtx;
+  SDL_Thread *thr;
+  char *board_ref;
   Turn search_color;
+  Move best;
+  bool thinking;
 } Sockfish;
 
 void sf_init(Sockfish *sf);
+void sf_req_search(Sockfish *sf);
 void sf_destroy(Sockfish *sf);
