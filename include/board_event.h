808884d32f832688b15700b77a83c8feea021d19
DrShahinstein <1taylanozturk@protonmail.com>
2025-08-17 12:46:49 +0300

    Pass ui to board_handle_event() in board_event.h

--- include/board_event.h
+++ include/board_event.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "board.h"
+#include "ui.h" // UI_State
 #include <SDL3/SDL.h>
 
-void board_handle_event(SDL_Event *e, BoardState *board);
+void board_handle_event(SDL_Event *e, BoardState *board, UI_State *ui);
