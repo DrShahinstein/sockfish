9e422c2a7d2cc158693b82c52eaf40cbf015ef46
DrShahinstein <1taylanozturk@protonmail.com>
2025-08-18 19:02:33 +0300

    Create castlings for BoardState in board.h

--- include/board.h
+++ include/board.h
@@ -3,13 +3,19 @@
 #define SQ 100
 #define BOARD_SIZE (SQ * 8)
 #define PIECE_PATH "assets/pieces/%c.png"
-#define START_FEN "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+#define START_FEN "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
 #include <SDL3/SDL.h>
+
+#define CASTLE_WK 0x01
+#define CASTLE_WQ 0x02
+#define CASTLE_BK 0x04
+#define CASTLE_BQ 0x08
 
 typedef struct BoardState {
   bool running;
   char board[8][8];
+  uint8_t castling;
   SDL_Texture *tex[128];
   struct {
     int row;
