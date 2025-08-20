23975a705836fadbfec4f7fcd854c20fb051633b
DrShahinstein <1taylanozturk@protonmail.com>
2025-08-20 22:07:15 +0300

    Create special_moves.c

---
+++ src/special_moves.c
@@ -0,0 +1,78 @@
+#include "special_moves.h"
+
+void update_castling_rights(BoardState *board, char moving_piece, Move *move) {
+  if (moving_piece == 'K')      board->castling &= ~(CASTLE_WK | CASTLE_WQ);
+  else if (moving_piece == 'k') board->castling &= ~(CASTLE_BK | CASTLE_BQ);
+
+  else if (moving_piece == 'R') {
+    if (move->fr == 7 && move->fc == 0)      board->castling &= ~CASTLE_WQ;
+    else if (move->fr == 7 && move->fc == 7) board->castling &= ~CASTLE_WK;
+  }
+  
+  else if (moving_piece == 'r') {
+    if (move->fr == 0 && move->fc == 0)      board->castling &= ~CASTLE_BQ;
+    else if (move->fr == 0 && move->fc == 7) board->castling &= ~CASTLE_BK;
+  }
+}
+
+bool is_castling_move(BoardState *board, Move *move) {
+  char piece = board->board[move->fr][move->fc];
+
+  if ((piece != 'K' && piece != 'k') || (move->fr != move->tr) || SDL_abs(move->fc - move->tc) != 2) {
+    return false;
+  }
+
+  bool kingside = (move->tc > move->fc);
+  int rook_col  = kingside ? 7:0;
+  char rook     = (piece == 'K') ? 'R':'r';
+
+  if (board->board[move->fr][rook_col] != rook) {
+    return false;
+  }
+
+  uint8_t required_right;
+  if (piece == 'K') {
+    required_right = kingside ? CASTLE_WK : CASTLE_WQ;
+  } else {
+    required_right = kingside ? CASTLE_BK : CASTLE_BQ;
+  }
+
+  return (board->castling & required_right) != 0;
+}
+
+void perform_castling(BoardState *board, Move *move) {
+  char piece      = board->board[move->fr][move->fc];
+  bool kingside   = (move->tc > move->fc);
+  int rook_fr_col = kingside ? 7 : 0;
+  int rook_to_col = kingside ? move->tc-1 : move->tc+1;
+
+  board->board[move->tr][move->tc] = piece;
+  board->board[move->fr][move->fc] = 0;
+
+  char rook = board->board[move->fr][rook_fr_col];
+  board->board[move->fr][rook_to_col] = rook;
+  board->board[move->fr][rook_fr_col] = 0;
+
+  if (piece == 'K') {
+    board->castling &= ~(CASTLE_WK | CASTLE_WQ);
+  } else {
+    board->castling &= ~(CASTLE_BK | CASTLE_BQ);
+  }
+}
+
+bool is_en_passant_capture(BoardState *board, Move *move) {
+  char piece = board->board[move->fr][move->fc];
+  
+  if ((piece != 'p' && piece != 'P') || move->fc == move->tc || board->board[move->tr][move->tc] != 0) {
+    return false;
+  }
+  
+  return (move->tr == board->ep_row && move->tc == board->ep_col);
+}
+
+void update_enpassant_rights(BoardState *board, char moving_piece) {
+  if (moving_piece != 'p' && moving_piece != 'P') {
+    board->ep_row = -1;
+    board->ep_col = -1;
+  }
+}
