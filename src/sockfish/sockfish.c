40b24be859baf6e03cf6d9996d182cd4b6b7fde9
DrShahinstein <1taylanozturk@protonmail.com>
2025-08-22 19:08:23 +0300

    Update placeholder search code in sockfish.c

--- src/sockfish/sockfish.c
+++ src/sockfish/sockfish.c
@@ -4,11 +4,16 @@
 Move sf_search(const SF_Context *ctx) {
   (void)ctx;
 
+  int a = SDL_rand(8) + 1;
+  int b = SDL_rand(8) + 1;
+  int c = SDL_rand(8) + 1;
+  int d = SDL_rand(8) + 1;
+
   for (int i = 0; i < 4; ++i) {
     if (i==0) SDL_Delay(100);
     SDL_Log("Searching for %s... [%d/4]", ctx->search_color == WHITE ? "White" : "Black", i+1);
     if (i!=3) SDL_Delay(100);
   }
 
-  return (Move){0};
+  return (Move){a,b,c,d};
 }
