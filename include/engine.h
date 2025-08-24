ddab458014801e62ef983bef4794f78671e4399f
DrShahinstein <1taylanozturk@protonmail.com>
2025-08-24 13:37:40 +0300

    Write explanatory comment section in engine.h to describe software design idea

--- include/engine.h
+++ include/engine.h
@@ -16,3 +16,21 @@ typedef struct EngineWrapper {
 void engine_init(EngineWrapper *engine);
 void engine_req_search(EngineWrapper *engine, const BoardState *board);
 void engine_destroy(EngineWrapper *engine);
+
+/*
+
+  EngineWrapper: A wrapper for the chess engine context and threading.
+  It provides a bridge between the main application and the chess engine (in this case, Sockfish).
+  This way, Sockfish is completely isolated from the rest of the application. Being a standalone package on its own.
+
+  - mtx: Mutex for thread safety.
+  - thr: Thread handle for the engine's search thread.
+  - ctx: The Sockfish engine context, holding the board state and search parameters. (!)
+  - last_pos_hash: Hash of the last searched position to avoid redundant searches.
+  - last_turn: The turn color of the last searched position.
+
+  So, 'Engine' and 'Sockfish' are two different concepts.
+  'Engine' is a constant bridge module in this application, which connects 'Sockfish' into the project.
+  And, 'Sockfish' is a standalone chess engine module. Regards search algorithms, depth and so on...
+
+*/
