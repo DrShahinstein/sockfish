00f0944f873b4e84b0b2120f35b7413905d3be23
DrShahinstein <1taylanozturk@protonmail.com>
2025-08-30 14:45:40 +0300

    Implement helper bit functions in bitboard.c

--- src/engine/sockfish/bitboard.c
+++ src/engine/sockfish/bitboard.c
@@ -12,3 +12,19 @@ void print_bitboard(Bitboard bb) {
   }
   printf("   a b c d e f g h\n");
 }
+
+inline int pop_lsb(U64 *bb) {
+  if (*bb == 0) return -1;
+  int index = __builtin_ctzll(*bb);
+  *bb &= *bb - 1;
+  return index;
+}
+
+inline int get_lsb(U64 bb) {
+  if (bb == 0) return -1;
+  return __builtin_ctzll(bb);
+}
+
+inline int count_bits(U64 bb) {
+  return __builtin_popcountll(bb);
+}
