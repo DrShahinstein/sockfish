55e73146a4c1bed205a96e4c13fb8f3ee050a670
DrShahinstein <1taylanozturk@protonmail.com>
2025-08-30 19:45:47 +0300

    Suppress 'unused' warnings of compiler for bit utils in bitboard.c

--- src/engine/sockfish/bitboard.c
+++ src/engine/sockfish/bitboard.c
@@ -1,7 +1,7 @@
 #include "bitboard.h"
 #include <stdio.h>
 
-void print_bitboard(Bitboard bb) {
+void __attribute__((unused)) print_bitboard(Bitboard bb) {
   for (int rank = 7; rank >= 0; rank--) {
     printf("%d ", rank + 1);
     for (int file = 0; file < 8; file++) {
@@ -13,18 +13,18 @@ void print_bitboard(Bitboard bb) {
   printf("   a b c d e f g h\n");
 }
 
-inline int pop_lsb(U64 *bb) {
+inline __attribute__((unused)) int pop_lsb(U64 *bb) {
   if (*bb == 0) return -1;
   int index = __builtin_ctzll(*bb);
   *bb &= *bb - 1;
   return index;
 }
 
-inline int get_lsb(U64 bb) {
+inline __attribute__((unused)) int get_lsb(U64 bb) {
   if (bb == 0) return -1;
   return __builtin_ctzll(bb);
 }
 
-inline int count_bits(U64 bb) {
+inline __attribute__((unused)) int count_bits(U64 bb) {
   return __builtin_popcountll(bb);
 }
