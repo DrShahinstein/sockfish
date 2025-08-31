6e8f9142d64aa2fc6027e24eea017959709300c5
DrShahinstein <1taylanozturk@protonmail.com>
2025-08-31 12:46:15 +0300

    Remove placeholder code of search.c

--- src/engine/sockfish/search.c
+++ src/engine/sockfish/search.c
@@ -1,19 +1,9 @@
-#include "sockfish.h"
-#include <SDL3/SDL.h> // temp
+#include "sockfish/search.h"
 
 Move sf_search(const SF_Context *ctx) {
   (void)ctx;
 
-  int a = SDL_rand(8) + 1;
-  int b = SDL_rand(8) + 1;
-  int c = SDL_rand(8) + 1;
-  int d = SDL_rand(8) + 1;
+  // codecodecode
 
-  for (int i = 0; i < 4; ++i) {
-    if (i==0) SDL_Delay(100);
-    SDL_Log("Searching for %s... [%d/4]", ctx->search_color == WHITE ? "White" : "Black", i+1);
-    if (i!=3) SDL_Delay(100);
-  }
-
-  return (Move){a,b,c,d};
+  return (Move){0};
 }
