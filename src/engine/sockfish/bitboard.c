9a27118a0163d521322f3200975dff118f424e6f
DrShahinstein <1taylanozturk@protonmail.com>
2025-09-01 16:13:58 +0300

    Remove bit position utils from bitboard.c (moved to bitboard.h as macros)

--- src/engine/sockfish/bitboard.c
+++ src/engine/sockfish/bitboard.c
@@ -12,19 +12,3 @@ void __attribute__((unused)) print_bitboard(Bitboard bb) {
   }
   printf("   a b c d e f g h\n");
 }
-
-inline __attribute__((unused)) int pop_lsb(U64 *bb) {
-  if (*bb == 0) return -1;
-  int index = __builtin_ctzll(*bb);
-  *bb &= *bb - 1;
-  return index;
-}
-
-inline __attribute__((unused)) int get_lsb(U64 bb) {
-  if (bb == 0) return -1;
-  return __builtin_ctzll(bb);
-}
-
-inline __attribute__((unused)) int count_bits(U64 bb) {
-  return __builtin_popcountll(bb);
-}
