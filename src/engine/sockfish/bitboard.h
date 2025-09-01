b558578bc83cb48da0eb091597c2393d697b1e96
DrShahinstein <1taylanozturk@protonmail.com>
2025-09-01 16:13:32 +0300

    Move bit pos funcs from bitboard.c to bitboard.h defining them as macros

--- src/engine/sockfish/bitboard.h
+++ src/engine/sockfish/bitboard.h
@@ -20,7 +20,15 @@ typedef struct {
 #define GET_BIT(bb, square)   ((bb) & (1ULL << (square)))
 #define POP_BIT(bb, square)   (GET_BIT(bb, square) ? (CLEAR_BIT(bb, square), 1) : 0)
 
+#define GET_LSB(bb)    (__builtin_ctzll(bb))
+#define GET_MSB(bb)    (__builtin_clzll(bb))
+#define COUNT_BITS(bb) (__builtin_popcountll(bb))
+
+static inline int POP_LSB(U64 *bb) {
+  if (*bb == 0) return -1;
+  int index = GET_LSB(*bb);
+  *bb &= *bb - 1;
+  return index;
+}
+
 void print_bitboard(Bitboard bb);
-int pop_lsb(U64 *bb);
-int get_lsb(U64 bb);
-int count_bits(U64 bb);
