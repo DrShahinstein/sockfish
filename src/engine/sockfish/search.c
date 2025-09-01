962a5fdad6e7e827d1df2ea84e6eebc2e90826d4
DrShahinstein <1taylanozturk@protonmail.com>
2025-09-01 23:51:57 +0300

    Write temporary char map for 'enum Square' convertions in search.c

--- src/engine/sockfish/search.c
+++ src/engine/sockfish/search.c
@@ -10,7 +10,17 @@ MoveSQ sf_search(const SF_Context *ctx) {
 
   for (int i = 0; i < generated_moves.count; ++i) {
     MoveSQ move = generated_moves.moves[i];
-    printf("Move %d: from (%d) to (%d)\n", i+1, move.from, move.to);
+    static const char *square_names[] = { //temp
+      "a1","b1","c1","d1","e1","f1","g1","h1", //temp
+      "a2","b2","c2","d2","e2","f2","g2","h2", //temp
+      "a3","b3","c3","d3","e3","f3","g3","h3", //temp
+      "a4","b4","c4","d4","e4","f4","g4","h4", //temp
+      "a5","b5","c5","d5","e5","f5","g5","h5", //temp
+      "a6","b6","c6","d6","e6","f6","g6","h6", //temp
+      "a7","b7","c7","d7","e7","f7","g7","h7", //temp
+      "a8","b8","c8","d8","e8","f8","g8","h8"  //temp
+    };
+    printf("Move %d: from %s to %s\n", i+1, square_names[move.from], square_names[move.to]);
   }
 
   printf("\n=> Generated for %s\n", ctx->search_color == WHITE ? "WHITE" : "BLACK");
