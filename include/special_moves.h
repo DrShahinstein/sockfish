63a6aaa9dc3b603f2821888df6f31666616b9708
DrShahinstein <1taylanozturk@protonmail.com>
2025-09-05 16:11:23 +0300

    Fix params of special_moves.h headers in accordance with new 'Move' type

--- include/special_moves.h
+++ include/special_moves.h
@@ -2,8 +2,8 @@
 
 #include "board.h"
 
-void update_castling_rights(BoardState *board, char moving_piece, MoveRC *move);
-bool is_castling_move(BoardState *board, MoveRC *move);
-void perform_castling(BoardState *board, MoveRC *move);
-bool is_en_passant_capture(BoardState *board, MoveRC *move);
+void update_castling_rights(BoardState *board, char moving_piece, Move move);
+bool is_castling_move(BoardState *board, Move move);
+void perform_castling(BoardState *board, Move move);
+bool is_en_passant_capture(BoardState *board, Move move);
 void update_enpassant_rights(BoardState *board, char moving_piece);
