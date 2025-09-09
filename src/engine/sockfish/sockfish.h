005dc65d78e6fcd911a390c978fe8f82130b41ac
DrShahinstein <1taylanozturk@protonmail.com>
2025-09-09 20:58:08 +0300

    Create PieceType enum in sockfish.h

--- src/engine/sockfish/sockfish.h
+++ src/engine/sockfish/sockfish.h
@@ -14,6 +14,12 @@
 #include <stdbool.h>
 
 typedef enum {
+  W_PAWN, W_KNIGHT, W_BISHOP, W_ROOK, W_QUEEN, W_KING,
+  B_PAWN, B_KNIGHT, B_BISHOP, B_ROOK, B_QUEEN, B_KING,
+  NO_PIECE=-1
+} PieceType;
+
+typedef enum {
   A1, B1, C1, D1, E1, F1, G1, H1,
   A2, B2, C2, D2, E2, F2, G2, H2,
   A3, B3, C3, D3, E3, F3, G3, H3,
