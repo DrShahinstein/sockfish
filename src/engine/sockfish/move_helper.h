1212be52b4582155e93e77eb3e7e04884ba65a89
DrShahinstein <1taylanozturk@protonmail.com>
2025-09-09 20:57:49 +0300

    Create move_helper.h

---
+++ src/engine/sockfish/move_helper.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "sockfish/sockfish.h"
+
+typedef struct {
+  Move move;
+  PieceType captured_piece;
+  Square captured_square;
+  uint8_t prev_castling; // previous castling rights
+  Square prev_ep_sq;
+} MoveHistory;
+
+void make_move(SF_Context *ctx, Move move, MoveHistory *history);
+void unmake_move(SF_Context *ctx, const MoveHistory *history);
+bool king_in_check(const SF_Context *ctx, Turn color);
+
+/*
+
+MoveList sf_generate_moves(SF_Context *ctx); // movegen.h
+
+=> 'move_helper.h' enables the definition of this function in 'movegen.c'
+
+*/
