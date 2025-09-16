058c2ca5bc0f86d89ff17814c63203f46cb6e65c
DrShahinstein <1taylanozturk@protonmail.com>
2025-09-16 19:23:47 +0300

    Improve UI look with readable implementation in ui.c

--- src/ui/ui.c
+++ src/ui/ui.c
@@ -6,23 +6,23 @@
 void ui_init(UI_State *ui) {
   ui->font                    = TTF_OpenFont(ROBOTO,16);
   ui->engine_on               = false;
-  ui->engine_toggler.rect     = (SDL_FRect){BOARD_SIZE+UI_PADDING,UI_PADDING,30,30};
+  ui->engine_toggler.rect     = (SDL_FRect){UI_START_X, UI_START_Y, 30, 30};
   ui->engine_toggler.hovered  = false;
   ui->fen_loader.font         = TTF_OpenFont(JBMONO,14);
-  ui->fen_loader.area.rect    = (SDL_FRect){BOARD_SIZE+UI_PADDING,UI_PADDING+50,230,90};
+  ui->fen_loader.area.rect    = (SDL_FRect){UI_MIDDLE-115, UI_START_Y+50, UI_FILLER_W, 90};
   ui->fen_loader.active       = false;
   ui->fen_loader.area.hovered = false; 
   ui->fen_loader.length       = 0;
   ui->fen_loader.input[0]     = '\0';
-  ui->fen_loader.btn.rect     = (SDL_FRect){ui->fen_loader.area.rect.x,ui->fen_loader.area.rect.y+100,100,30};
+  ui->fen_loader.btn.rect     = (SDL_FRect){UI_MIDDLE-50, ui->fen_loader.area.rect.y + 95, 100, 30};
   ui->fen_loader.btn.hovered  = false;
-  ui->reset_btn.rect          = (SDL_FRect){ui->fen_loader.area.rect.x+ui->fen_loader.area.rect.w-100,ui->fen_loader.btn.rect.y,100,ui->fen_loader.btn.rect.h};
-  ui->undo_btn.rect           = (SDL_FRect){BOARD_SIZE + UI_WIDTH/2 - 50, BOARD_SIZE-50, 100, 30};
-  ui->undo_btn.hovered        = false;
-  ui->reset_btn.hovered       = false;
-  ui->separator.rect          = (SDL_FRect){ui->fen_loader.area.rect.x,ui->reset_btn.rect.y+80,ui->fen_loader.area.rect.w,4};
-  ui->turn_changer.rect       = (SDL_FRect){ui->fen_loader.area.rect.x,ui->separator.rect.y+25,30,30};
+  ui->separator.rect          = (SDL_FRect){UI_START_X, ui->fen_loader.btn.rect.y + 50, UI_FILLER_W, 4};
+  ui->turn_changer.rect       = (SDL_FRect){UI_START_X, ui->separator.rect.y + 25, 30, 30};
   ui->turn_changer.hovered    = false;
+  ui->undo_btn.rect           = (SDL_FRect){UI_MIDDLE-50, BOARD_SIZE-80, 100, 30};
+  ui->undo_btn.hovered        = false;
+  ui->reset_btn.rect          = (SDL_FRect){UI_MIDDLE-50, BOARD_SIZE-40, 100, 30};
+  ui->reset_btn.hovered       = false;
 
   if (!ui->font)            SDL_Log("Could not load font: %s", SDL_GetError());
   if (!ui->fen_loader.font) SDL_Log("Could not load font: %s", SDL_GetError());
