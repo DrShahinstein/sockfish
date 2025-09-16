2bba8d48175d76c59d077d3bea76afe05bd579c7
DrShahinstein <1taylanozturk@protonmail.com>
2025-09-16 16:04:18 +0300

    Create BoardMoveHistory structure and history funcs in board.h

--- include/board.h
+++ include/board.h
@@ -4,10 +4,26 @@
 #define BOARD_SIZE (SQ * 8)
 #define PIECE_PATH "assets/pieces/%c.png"
 #define START_FEN "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+#define MAX_HISTORY 128
 
 #include "sockfish/sockfish.h" /* Move, Turn, CASTLE_WK, CASTLE_WQ, CASTLE_BK, CASTLE_BQ, '= Move Utilities =' ... */
 #include "sockfish/movegen.h"  /* MoveList, sf_generate_moves() */
 #include <SDL3/SDL.h>
+
+typedef struct {
+  int from_row;
+  int from_col;
+  int to_row;
+  int to_col;
+  char moving_piece;
+  char captured_piece;
+  int captured_row;
+  int captured_col;
+  uint8_t prev_castling;
+  int prev_ep_row;
+  int prev_ep_col;
+  Turn prev_turn;
+} BoardMoveHistory;
 
 typedef struct {
   bool active;
@@ -41,8 +57,12 @@ typedef struct BoardState {
   Promotion promo;
   int ep_row; // en-passant row (-1 for none)
   int ep_col; // en-passant col (-1 for none)
+  BoardMoveHistory history[MAX_HISTORY];
+  int history_count;
 } BoardState ;
 
 void load_fen(const char *fen, BoardState *board);
 void load_board(const char *fen, BoardState *board);
 void board_init(BoardState *board);
+void board_save_history(BoardState *board, int from_row, int from_col, int to_row, int to_col);
+void board_undo(BoardState *board);
