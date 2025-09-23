d7b7b0ee288465acfedba4abde58f0cc4ce30867
DrShahinstein <1taylanozturk@protonmail.com>
2025-09-23 22:10:39 +0300

    Initialize and cleanup line buffers and cache for text inputs in ui.c

--- src/ui/ui.c
+++ src/ui/ui.c
@@ -3,13 +3,8 @@
 #include <SDL3/SDL.h>
 #include <SDL3_ttf/SDL_ttf.h>
 
-static void init_text_input_buffer(UI_TextInput *text_inp, size_t max_len) {
-  text_inp->buf    = SDL_malloc(max_len * sizeof(char));
-  text_inp->buf[0] = '\0';
-}
-static void cleanup_text_input_buffer(UI_TextInput *text_inp) {
-  SDL_free(text_inp->buf);
-}
+static void init_text_input_buffers(UI_TextInput *text_inp, size_t max_len);
+static void cleanup_text_input_buffers(UI_TextInput *text_inp);
 
 void ui_init(UI_State *ui) {
   FontMenu fonts = {
@@ -47,10 +42,10 @@ void ui_init(UI_State *ui) {
   ui->reset_btn.hovered       = false;
 
   SDL_strlcpy(ui->fen_loader.placeholder, FEN_PLACEHOLDER, sizeof(ui->fen_loader.placeholder));
-  init_text_input_buffer(&ui->fen_loader, MAX_FEN);
+  init_text_input_buffers(&ui->fen_loader, MAX_FEN);
 
   SDL_strlcpy(ui->pgn_loader.placeholder, PGN_PLACEHOLDER, sizeof(ui->pgn_loader.placeholder));
-  init_text_input_buffer(&ui->pgn_loader, MAX_PGN);
+  init_text_input_buffers(&ui->pgn_loader, MAX_PGN);
 
   if (!ui->fonts.roboto16) SDL_Log("Could not load font: %s", SDL_GetError());
   if (!ui->fonts.roboto15) SDL_Log("Could not load font: %s", SDL_GetError());
@@ -58,8 +53,8 @@ void ui_init(UI_State *ui) {
 }
 
 void ui_destroy(UI_State *ui) {
-  cleanup_text_input_buffer(&ui->fen_loader);
-  cleanup_text_input_buffer(&ui->pgn_loader);
+  cleanup_text_input_buffers(&ui->fen_loader);
+  cleanup_text_input_buffers(&ui->pgn_loader);
 
   TTF_CloseFont(ui->fonts.roboto16);
   ui->fonts.roboto16 = NULL;
@@ -71,4 +66,38 @@ void ui_destroy(UI_State *ui) {
   ui->fonts.jbmono14 = NULL;
 
   SDL_StopTextInput(SDL_GetKeyboardFocus());
+}
+
+static void init_text_input_buffers(UI_TextInput *text_inp, size_t max_len) {
+  text_inp->buf               = SDL_malloc(max_len * sizeof(char));
+  text_inp->buf[0]            = '\0';
+  text_inp->cached_line_count = 0;
+  text_inp->cached_text_hash  = 0;
+  text_inp->cached_wrap_width = 0;
+  text_inp->cache_valid       = false;
+
+  int max_lines    = (text_inp->type == FEN) ? 3 : 10;
+  int max_buf_size = (text_inp->type == FEN) ? MAX_FEN : MAX_PGN;
+
+  text_inp->wrap_lines      = SDL_malloc(max_lines * sizeof(char *));
+  text_inp->wrap_line_count = max_lines;
+
+  for (int i = 0; i < max_lines; ++i) {
+    text_inp->wrap_lines[i]    = SDL_malloc(max_buf_size);
+    text_inp->wrap_lines[i][0] = '\0';
+  }
+}
+
+static void cleanup_text_input_buffers(UI_TextInput *text_inp) {
+  SDL_free(text_inp->buf);
+
+  if (text_inp->wrap_lines) {
+    for (int i = 0; i < text_inp->wrap_line_count; ++i) {
+      SDL_free(text_inp->wrap_lines[i]);
+    }
+
+    SDL_free(text_inp->wrap_lines);
+    text_inp->wrap_lines      = NULL;
+    text_inp->wrap_line_count = 0;
+  }
 }
