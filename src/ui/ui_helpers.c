d5f2cb9cbc96fb83f86dbd688b16387b8f26f609
DrShahinstein <1taylanozturk@protonmail.com>
2025-09-23 22:17:01 +0300

    Fix wrap_text_into_lines() and modify render_text_input() to use cache-able wrap lines in ui_helpers.c

--- src/ui/ui_helpers.c
+++ src/ui/ui_helpers.c
@@ -3,6 +3,8 @@
 #include <SDL3/SDL.h>
 #include <SDL3_ttf/SDL_ttf.h>
 
+static size_t hash_string(const char *str);
+
 void draw_text(SDL_Renderer *r, TTF_Font *font, const char *text, SDL_Color color, float x, float y) {
   float width, height;
   SDL_Texture *tex = get_text_cache(font, text, color, &width, &height);
@@ -33,42 +35,54 @@ int wrap_text_into_lines(TTF_Font *font, const char *text, float max_w, char **l
   int line_count = 0;
 
   while (start < len && line_count < max_lines) {
-    int end   = start;
-    int w     = 0;
-    char *tmp = (char *)SDL_malloc(max_buf_size);
-
-    if (!tmp)
-      break;
+    int end        = start;
+    int last_space = -1;
+    int w          = 0;
 
     while (end < len) {
-      int chunk = end - start + 1;
-      if (chunk >= max_buf_size) break;
+      if (text[end] == ' ') {
+        last_space = end;
+      }
 
-      SDL_strlcpy(tmp, &text[start], chunk + 1);
-      tmp[chunk] = '\0';
+      int chunk_len = end - start + 1;
+      if (chunk_len >= max_buf_size) break;
 
-      TTF_MeasureString(font, tmp, 0, 0, &w, NULL);
-      if (w > (int)max_w)
+      char temp_char = text[end + 1];
+      ((char *)text)[end + 1] = '\0';
+
+      TTF_MeasureString(font, &text[start], 0, 0, &w, NULL);
+
+      ((char *)text)[end + 1] = temp_char;
+
+      if (w > (int)max_w) {
+        if (last_space > start && last_space < end) {
+          end = last_space;
+        }
         break;
+      }
 
       end++;
     }
 
-    if (end == start) {
-      int take = 1;
-      if (start + take > len) take = len - start;
-      SDL_strlcpy(lines[line_count], &text[start], SDL_min(take + 1, max_buf_size));
+    if (end > start) {
+      int copy_len = SDL_min(end - start, max_buf_size - 1);
+      SDL_strlcpy(lines[line_count], &text[start], copy_len + 1);
       line_count++;
-      start += take;
-    } else {
-      int take = end - start;
-      if (take >= max_buf_size) take = max_buf_size - 1;
-      SDL_strlcpy(lines[line_count], &text[start], take + 1);
-      line_count++;
+
+      if (start < len && text[end] == ' ') end++;
       start = end;
     }
-
-    SDL_free(tmp);
+    
+    else {
+      if (start < len) {
+        lines[line_count][0] = text[start];
+        lines[line_count][1] = '\0';
+        line_count++;
+        start++;
+      } else {
+        break;
+      }
+    }
   }
 
   return line_count;
@@ -76,75 +90,94 @@ int wrap_text_into_lines(TTF_Font *font, const char *text, float max_w, char **l
 
 void render_text_input(SDL_Renderer *r, UI_TextInput *ui_text_input, FontMenu *fonts) {
   SDL_FRect rect = ui_text_input->area.rect;
+
   SDL_SetRenderDrawColor(r, 255, 255, 255, 0);
   SDL_RenderFillRect(r, &rect);
   SDL_SetRenderDrawColor(r, 0, 0, 0, 255);
   SDL_RenderRect(r, &rect);
 
-  float text_x = rect.x + 6;
-  float text_y = rect.y + 6;
+  float text_x     = rect.x + 6;
+  float text_y     = rect.y + 6;
+  float wrap_width = rect.w - 8.0f;
 
   int line_h = TTF_GetFontHeight(fonts->jbmono14);
   if (line_h <= 0) line_h = 16;
 
-  int max_vis_lines = (int) (rect.h / (float)line_h);
+  int max_vis_lines = (int)(rect.h / (float)line_h);
   if (max_vis_lines <= 0) max_vis_lines = 1;
 
-  const int MAX_WRAP_LINES = (ui_text_input->type == FEN) ? 3 : 10;
-  int max_buf_size         = (ui_text_input->type == FEN) ? MAX_FEN : MAX_PGN;
-  char **wrapped           = (char **)SDL_malloc(MAX_WRAP_LINES * sizeof(char *));
+  // check if we need to recalculate wrapping (saves cpu overhead)
+  size_t current_hash = hash_string(ui_text_input->buf);
+  bool needs_rewrap   = !ui_text_input->cache_valid                      ||
+                        ui_text_input->cached_text_hash  != current_hash ||
+                        ui_text_input->cached_wrap_width != wrap_width;
 
-  for (int i = 0; i < MAX_WRAP_LINES; ++i) {
-    wrapped[i]    = (char *)SDL_malloc(max_buf_size);
-    wrapped[i][0] = '\0';
+  int total_lines;
+
+  if (needs_rewrap) {
+    for (int i = 0; i < ui_text_input->wrap_line_count; ++i) {
+      ui_text_input->wrap_lines[i][0] = '\0';
+    }
+
+    int max_buf_size = (ui_text_input->type == FEN) ? MAX_FEN : MAX_PGN;
+
+    total_lines =
+        wrap_text_into_lines(fonts->jbmono14, ui_text_input->buf, wrap_width,
+                             ui_text_input->wrap_lines,
+                             ui_text_input->wrap_line_count, max_buf_size);
+
+    ui_text_input->cached_line_count = total_lines;
+    ui_text_input->cached_text_hash  = current_hash;
+    ui_text_input->cached_wrap_width = wrap_width;
+    ui_text_input->cache_valid       = true;
+  } else {
+    total_lines = ui_text_input->cached_line_count;
   }
-
-  int total_lines
-    = wrap_text_into_lines(fonts->jbmono14, ui_text_input->buf, rect.w - 8.0f, wrapped, MAX_WRAP_LINES, max_buf_size);
 
   int first_line = 0;
   if (total_lines > max_vis_lines) {
-    if (ui_text_input->active) first_line = total_lines - max_vis_lines;
-    else first_line = 0;
+    if (ui_text_input->active) {
+      first_line = total_lines - max_vis_lines;
+    }
   }
 
-  int last_line = first_line + max_vis_lines;
-  if (last_line > total_lines) last_line = total_lines;
-
-  for (int li = first_line, row_i = 0; li < last_line; ++li, ++row_i) {
-    float y = text_y + row_i * (float)line_h;
-    draw_text(r, fonts->jbmono14, wrapped[li], FBLACK, text_x, y);
-  }
+  int last_line = SDL_min(first_line + max_vis_lines, total_lines);
 
   if (ui_text_input->length == 0 && !ui_text_input->active) {
     draw_text(r, fonts->roboto15, ui_text_input->placeholder, FGRAY, text_x, text_y);
   }
-
-  else if (ui_text_input->length > 0) {
-    draw_text(r, fonts->jbmono14, ui_text_input->buf, FBLACK, text_x, text_y);
+  
+  else {
+    for (int li = first_line, row_i = 0; li < last_line; ++li, ++row_i) {
+      float y = text_y + row_i * (float)line_h;
+      draw_text(r, fonts->jbmono14, ui_text_input->wrap_lines[li], FBLACK, text_x, y);
+    }
   }
 
-  else {}
+  if (ui_text_input->active && (SDL_GetTicks() % 1000 < 500)) {
+    int visible_count = last_line - first_line;
+    int last_visible_idx = (visible_count > 0) ? (last_line - 1) : -1;
 
-  if (ui_text_input->active) {
-    int w; int visible_count;
-    bool caret_visible = SDL_GetTicks() % 1000 < 500;
-
-    if (caret_visible) {
-      visible_count = last_line - first_line;
-      int last_visible_idx = (visible_count > 0) ? (last_line - 1) : -1; 
-      w = 0;
-      if (last_visible_idx >= 0) TTF_MeasureString(fonts->jbmono14, wrapped[last_visible_idx], 0, 0, &w, NULL);
-      else w = 0;
+    int w = 0;
+    if (last_visible_idx >= 0 && ui_text_input->wrap_lines[last_visible_idx][0]) {
+      TTF_MeasureString(fonts->jbmono14, ui_text_input->wrap_lines[last_visible_idx], 0, 0, &w, NULL);
     }
 
-    float caret_x = text_x + (float)w;
-    float caret_y = text_y + (float) ((visible_count > 0 ? visible_count-1 : 0) * line_h);
-    SDL_FRect caret = { caret_x, caret_y, 2.0f, (float)line_h };
+    float caret_x   = text_x + (float)w;
+    float caret_y   = text_y + (float)((visible_count > 0 ? visible_count - 1 : 0) * line_h);
+    SDL_FRect caret = {caret_x, caret_y, 2.0f, (float)line_h};
     SDL_SetRenderDrawColor(r, 0, 0, 0, 255);
     SDL_RenderFillRect(r, &caret);
   }
+}
 
-  for (int i = 0; i < MAX_WRAP_LINES; ++i) SDL_free(wrapped[i]);
-  SDL_free(wrapped);
+static size_t hash_string(const char *str) {
+  size_t hash = 5381;
+  int c;
+
+  while ((c = *str++)) {
+    hash = ((hash << 5) + hash) + c;
+  }
+  
+  return hash;
 }
