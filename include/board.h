16965b6cb5ca2d72bd975527a972f7d9c039009c
DrShahinstein <1taylanozturk@protonmail.com>
2025-10-07 21:12:20 +0300

    Declare attribs for BoardState to cache king_in_check in board.h

--- include/board.h
+++ include/board.h
@@ -59,13 +59,16 @@ typedef struct BoardState {
   int ep_row;                                                                     // en-passant row (-1 for none)
   int ep_col;                                                                     // en-passant col (-1 for none)
   BoardMoveHistory history[MAX_HISTORY]; int undo_count; int redo_count;
+  struct { bool in_check; Turn color; int row; int col; } king;
 } BoardState ;
 
 void board_init(BoardState *board);
 void board_handle_event(SDL_Event *e, BoardState *board);
+void board_update_king_in_check(BoardState *b);
 void board_update_valid_moves(BoardState *b);
 void board_save_history(BoardState *board, int from_row, int from_col, int to_row, int to_col);
 void board_undo(BoardState *board);
 void board_redo(BoardState *board);
+
 void load_fen(const char *fen, BoardState *board);
 void load_pgn(const char *pgn, BoardState *board);
