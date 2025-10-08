ea62d265694be2ed867313ff8f0f7250022c26e2
DrShahinstein <1taylanozturk@protonmail.com>
2025-10-08 22:40:39 +0300

    Define 'Turn color' in func signature in parse_pgn_move.c

--- src/board/parse_pgn_move.c
+++ src/board/parse_pgn_move.c
@@ -1,10 +1,11 @@
 #include "board.h"
 
 /* Unimplemented Yet */
-void parse_pgn_move(const char *move, int *fr, int *fc, int *tr, int *tc) {
-  (void)move;
+void parse_pgn_move(const char *move, Turn color, int *fr, int *fc, int *tr, int *tc) {
+  (void)move; (void)color;
 
   SDL_Log("Move: %s", move);
+  SDL_Log("Turn: %d", color);
 
   *fr = 6; *fc = 4;
   *tr = 4; *tc = 4;
