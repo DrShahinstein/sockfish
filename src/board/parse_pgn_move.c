6d991156cb3fd7bb9061867dc6177f359cae89e5
DrShahinstein <1taylanozturk@protonmail.com>
2025-10-11 18:45:20 +0300

    Accept 'char (*squares)[8]' in parse_pgn_move() definition

--- src/board/parse_pgn_move.c
+++ src/board/parse_pgn_move.c
@@ -1,11 +1,8 @@
 #include "board.h"
 
 /* Unimplemented Yet */
-void parse_pgn_move(const char *move, Turn color, int *fr, int *fc, int *tr, int *tc) {
-  (void)move; (void)color;
-
-  SDL_Log("Move: %s", move);
-  SDL_Log("Turn: %d", color);
+void parse_pgn_move(const char *move, char (*squares)[8], Turn color, int *fr, int *fc, int *tr, int *tc) {
+  (void)move; (void)color; (void)squares;
 
   *fr = 6; *fc = 4;
   *tr = 4; *tc = 4;
