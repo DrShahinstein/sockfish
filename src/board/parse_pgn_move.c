b7dac6e6d6436ad7581c64f490a0ea92585402d2
DrShahinstein <1taylanozturk@protonmail.com>
2025-10-11 22:01:19 +0300

    Fix parse_pgn_move() to work out okay with new corrected params

--- src/board/parse_pgn_move.c
+++ src/board/parse_pgn_move.c
@@ -1,9 +1,12 @@
 #include "board.h"
 
 /* Unimplemented Yet */
-void parse_pgn_move(const char *move, char (*squares)[8], Turn color, int *fr, int *fc, int *tr, int *tc) {
-  (void)move; (void)color; (void)squares;
+void parse_pgn_move(const char *move, SF_Context *sf_ctx, char (*squares)[8], int *fr, int *fc, int *tr, int *tc) {
+  (void)move; (void)sf_ctx; (void)squares;
 
   *fr = 6; *fc = 4;
   *tr = 4; *tc = 4;
+
+  squares[*fr][*fc] = 0;
+  squares[*tr][*tc] = 'P';
 }
