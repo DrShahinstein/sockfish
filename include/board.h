7e451afde1934b4929964e79c43be4e18fde68c4
DrShahinstein <1taylanozturk@protonmail.com>
2025-11-26 00:34:28 +0300

    Declare 'Annotations' type for BoardState (board.h)

--- include/board.h
+++ include/board.h
@@ -5,6 +5,8 @@
 #define PIECE_PATH "assets/pieces/%c.png"
 #define START_FEN "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 #define MAX_HISTORY 512
+#define MAX_ARROWS 64
+#define MAX_HIGHLIGHTS 64
 
 #include "sockfish/sockfish.h" /* Move, Turn, CASTLE_WK, CASTLE_WQ, CASTLE_BK, CASTLE_BQ, '= Move Utilities =' ... */
 #include "sockfish/movegen.h"  /* MoveList, sf_generate_moves() */
@@ -26,6 +28,7 @@ typedef struct {
   Turn turn;
 } BoardMoveHistory;
 
+/* Internal Type For 'BoardState' */
 typedef struct {
   bool active;
   int row;
@@ -34,6 +37,7 @@ typedef struct {
   char captured;
 } Promotion;
 
+/* Internal Type For 'BoardState' */
 typedef struct {
   bool active;
   int from_row;
@@ -42,24 +46,54 @@ typedef struct {
   int to_col;
 } Drag;
 
+/* Internal Type For 'BoardState' */
 typedef struct {
   bool active;
   int row;
   int col;
 } SelectedPiece;
 
+/* Internal Type For 'BoardState' */
+typedef struct {
+  bool in_check;
+  Turn color;
+  int row;
+  int col;
+} King;
+
+/* Internal Type For 'Annotations' */
+typedef struct {
+  Square from;
+  Square to;
+  SDL_FColor color;
+} Arrow;
+
+/* Internal Type For 'Annotations' */
+typedef struct {
+  Square square;
+  SDL_FColor color;
+} Highlight;
+
+/* Internal Type For 'BoardState' */
+typedef struct {
+  Square arrow_start;                   bool drawing_arrow;
+  Arrow arrows[MAX_ARROWS];             int arrow_count;
+  Highlight highlights[MAX_HIGHLIGHTS]; int highlight_count;
+} Annotations;
+
 typedef struct BoardState {
   char board[8][8];
+  int ep_row;
+  int ep_col;
   uint8_t castling;
   Turn turn;
   MoveList valid_moves; bool should_update_valid_moves;
   SelectedPiece selected_piece;
   Drag drag;
   Promotion promo;
-  int ep_row;                                                                     // en-passant row (-1 for none)
-  int ep_col;                                                                     // en-passant col (-1 for none)
+  King king;
   BoardMoveHistory history[MAX_HISTORY]; int undo_count; int redo_count;
-  struct { bool in_check; Turn color; int row; int col; } king;
+  Annotations annotations;
 } BoardState ;
 
 void board_init(BoardState *board);
@@ -69,7 +103,8 @@ void board_update_valid_moves(BoardState *b);
 void board_save_history(BoardState *board, int from_row, int from_col, int to_row, int to_col, int history_index);
 void board_undo(BoardState *board);
 void board_redo(BoardState *board);
-
 void load_fen(const char *fen, BoardState *board);
 void load_pgn(const char *pgn, BoardState *board);
-void parse_pgn_move(const char *pgnmove, SF_Context *sf_ctx, char (*last_pos)[8], char *promote, int *fr, int *fc, int *tr, int *tc); // board/parse_pgn_move.c
+
+// parse_pgn_move.c
+void parse_pgn_move(const char *pgnmove, SF_Context *sf_ctx, char (*last_pos)[8], char *promote, int *fr, int *fc, int *tr, int *tc);
