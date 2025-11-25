f0cb26b9807ca4a25d1016790201e15304a530bd
DrShahinstein <1taylanozturk@protonmail.com>
2025-11-26 01:26:29 +0300

    Delete include/special_moves.h

--- include/special_moves.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "board.h"
-
-void update_castling_rights(BoardState *board, char moving_piece, Move move);
-bool is_castling_move(BoardState *board, Move move);
-void perform_castling(BoardState *board, Move move);
-bool is_en_passant_capture(BoardState *board, Move move);
-void update_enpassant_rights(BoardState *board, char moving_piece);
