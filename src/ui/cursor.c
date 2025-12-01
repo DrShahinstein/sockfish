9bc808b14f42872e7baf6b802ed9d26cd8857687
DrShahinstein <1taylanozturk@protonmail.com>
2025-12-02 00:17:10 +0300

    Implement get_mouse_pos() function (cursor.c)

--- src/ui/cursor.c
+++ src/ui/cursor.c
@@ -3,6 +3,7 @@
 SDL_Cursor *cursor_default = NULL;
 SDL_Cursor *cursor_pointer = NULL;
 SDL_Cursor *cursor_text    = NULL;
+SDL_Renderer *g_renderer   = NULL;
 
 void init_cursors(void) {
   cursor_default = SDL_CreateSystemCursor(SDL_SYSTEM_CURSOR_DEFAULT);
@@ -18,4 +19,16 @@ void cleanup_cursors(void) {
 
 inline bool cursor_in_rect(float x, float y, SDL_FRect *r) {
   return x >= r->x && x < (r->x + r->w) && y >= r->y && y < (r->y + r->h);
+}
+
+void get_mouse_pos(float *x, float *y)  {
+  float window_x, window_y;
+  SDL_GetMouseState(&window_x, &window_y);
+
+  if (g_renderer) {
+    SDL_RenderCoordinatesFromWindow(g_renderer, window_x, window_y, x, y);
+  } else {
+    *x = window_x;
+    *y = window_y;
+  }
 }
