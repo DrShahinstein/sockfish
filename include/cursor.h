a08ee1b050ee4fa2e622c8c19ab95c73e8694b99
DrShahinstein <1taylanozturk@protonmail.com>
2025-12-02 00:16:53 +0300

    Declare get_mouse_pos() function (cursor.h)

--- include/cursor.h
+++ include/cursor.h
@@ -10,3 +10,6 @@ extern SDL_Cursor *cursor_text;
 void init_cursors(void);
 void cleanup_cursors(void);
 bool cursor_in_rect(float x, float y, SDL_FRect *r);
+
+extern SDL_Renderer *g_renderer;
+void get_mouse_pos(float *x, float *y);
