224f7d63e8eed9bcff2016b9c0b427f962daf752
DrShahinstein <1taylanozturk@protonmail.com>
2025-12-22 20:59:17 +0300

    Update engine implementation for its safety with mutex usages (engine.c)

--- src/engine/engine.c
+++ src/engine/engine.c
@@ -1,10 +1,9 @@
 #include "engine.h"
+#include "board.h"
 #include "sockfish/sockfish.h"
 #include "sockfish/search.h"
 #include "sockfish/movegen.h"
-#include "board.h"
 #include <SDL3/SDL.h>
-#include <stdio.h>
 
 static int engine_thread(void *data);
 
@@ -15,76 +14,70 @@ void engine_init(EngineWrapper *engine) {
   /* init magic bitboards for sliding pieces in move generation logic */
   init_magic_bitboards();
 
-  SDL_SetAtomicInt(&engine->thr_working,    0);
-  SDL_SetAtomicInt(&engine->stop_requested, 0);
-
   engine->mtx           = SDL_CreateMutex();
   engine->cond          = SDL_CreateCondition();
   engine->last_pos_hash = 0ULL;
   engine->last_turn     = WHITE;
   engine->ctx           = create_sf_ctx(&(BitboardSet){0}, WHITE, CASTLE_ALL, NO_ENPASSANT);
+  engine->should_stop   = false;
+  engine->thr_working   = false;
   engine->thr           = SDL_CreateThread(engine_thread, "EngineThread", engine);
 }
 
 void engine_req_search(EngineWrapper *engine, const BoardState *board) {
-  if (!engine) return;
   if (board->promo.active) return;
 
-  if (SDL_GetAtomicInt(&engine->thr_working))
-    return;
-
-  uint64_t new_hash = position_hash(board->board, board->turn);
-  if (engine->last_pos_hash == new_hash && engine->last_turn == board->turn)
-    return;
-
   SDL_LockMutex(engine->mtx);
+  if (engine->thr_working) {
+    SDL_UnlockMutex(engine->mtx);
+    return;
+  }
 
-  if (SDL_GetAtomicInt(&engine->thr_working)) {
+  uint64_t hash = position_hash(board->board, board->turn);
+  if (engine->last_pos_hash == hash && engine->last_turn == board->turn) {
     SDL_UnlockMutex(engine->mtx);
     return;
   }
 
   bool ep_valid = board->ep_row >= 0 && board->ep_col >= 0;
-  
+
   BitboardSet bbset = make_bitboards_from_charboard(board->board);
   Square en_passant = ep_valid ? rowcol_to_sq(board->ep_row, board->ep_col) : NO_ENPASSANT;
   SF_Context ctx    = create_sf_ctx(&bbset, board->turn, board->castling, en_passant);
 
   engine->ctx           = ctx;
-  engine->last_pos_hash = new_hash;
+  engine->last_pos_hash = hash;
   engine->last_turn     = board->turn;
+  engine->thr_working   = true;
 
-  SDL_SetAtomicInt(&engine->thr_working, 1);
   SDL_SignalCondition(engine->cond);
   SDL_UnlockMutex(engine->mtx);
 }
 
 static int engine_thread(void *data) {
-  EngineWrapper *engine = (EngineWrapper *)(data);
-  if (!engine) return -1;
+  EngineWrapper *engine = data;
+  if (engine == NULL) return -1;
 
-  while (1) {
+  for (;;) {
     SDL_LockMutex(engine->mtx);
 
-    while (!SDL_GetAtomicInt(&engine->thr_working) && !SDL_GetAtomicInt(&engine->stop_requested)) {
+    while (!engine->thr_working && !engine->should_stop) {
       SDL_WaitCondition(engine->cond, engine->mtx);
     }
 
-    if (SDL_GetAtomicInt(&engine->stop_requested)) {
+    if (engine->should_stop) {
       SDL_UnlockMutex(engine->mtx);
       break;
     }
 
-    SF_Context ctx;
-    SDL_memcpy(&ctx, &engine->ctx, sizeof(SF_Context));
-    ctx.stop_requested = (bool*)&engine->stop_requested;
+    SF_Context ctx = engine->ctx; 
     SDL_UnlockMutex(engine->mtx);
 
     Move best = sf_search(&ctx);
 
     SDL_LockMutex(engine->mtx);
-    engine->ctx.best = best;
-    SDL_SetAtomicInt(&engine->thr_working, 0);
+    engine->ctx.best    = best;
+    engine->thr_working = false;
     SDL_UnlockMutex(engine->mtx);
   }
 
@@ -92,20 +85,18 @@ static int engine_thread(void *data) {
 }
 
 void engine_destroy(EngineWrapper *engine) {
-  SDL_SetAtomicInt(&engine->stop_requested, 1);
   SDL_LockMutex(engine->mtx);
+  engine->should_stop = true;
   SDL_SignalCondition(engine->cond);
   SDL_UnlockMutex(engine->mtx);
+
   SDL_WaitThread(engine->thr, NULL);
   SDL_DestroyCondition(engine->cond);
   SDL_DestroyMutex(engine->mtx);
 
-  engine->thr = NULL;
-  engine->mtx = NULL;
+  engine->thr  = NULL;
+  engine->mtx  = NULL;
   engine->cond = NULL;
-  
-  SDL_SetAtomicInt(&engine->thr_working, 0);
-  SDL_SetAtomicInt(&engine->stop_requested, 0);
 
   cleanup_magic_bitboards();
 }
