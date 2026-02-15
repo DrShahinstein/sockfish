efdeb9786a08ff95fe0b4b4828af29c9ddc937e5
DrShahinstein <1taylanozturk@protonmail.com>
2026-02-15 18:15:38 +0300

    Complete basic eval logic considering position and mobility (sockfish/evaluation.c)

--- src/engine/sockfish/evaluation.c
+++ src/engine/sockfish/evaluation.c
@@ -1,8 +1,9 @@
-#include "sockfish/sockfish.h"
 #include "sockfish/evaluation.h"
 #include "sockfish/bitboard.h"
+#include "sockfish/movegen.h"
 
 #define calc_color_offset(t) (t==0 ? +1 : -1)
+#define flip(s) (s ^ 56)
 
 static int calc_material_score(const SF_Context *ctx);
 static int calc_positional_score(const SF_Context *ctx);
@@ -39,11 +40,102 @@ static int calc_material_score(const SF_Context *ctx) {
 }
 
 static int calc_positional_score(const SF_Context *ctx) {
-  (void)(ctx);
-  return 0;
+  int score = 0;
+
+  const BitboardSet *bbset = &ctx->bitboard_set;
+  U64 pawns;
+  U64 knights;
+  U64 bishops;
+  U64 rooks;
+  U64 queens;
+  U64 kings;
+
+  pawns = bbset->pawns[WHITE];
+  while (pawns) {
+    int sq = POP_LSB(&pawns);
+    score += PAWN_TABLE[sq];
+  }
+
+  pawns = bbset->pawns[BLACK];
+  while (pawns) {
+    int sq = POP_LSB(&pawns);
+    score -= PAWN_TABLE[flip(sq)];
+  }
+
+  knights = bbset->knights[WHITE];
+  while (knights) {
+    int sq = POP_LSB(&knights);
+    score += KNIGHT_TABLE[sq];
+  }
+
+  knights = bbset->knights[BLACK];
+  while (knights) {
+    int sq = POP_LSB(&knights);
+    score -= KNIGHT_TABLE[flip(sq)];
+  }
+
+  bishops = bbset->bishops[WHITE];
+  while (bishops) {
+    int sq = POP_LSB(&bishops);
+    score += BISHOP_TABLE[sq];
+  }
+
+  bishops = bbset->bishops[BLACK];
+  while (bishops) {
+    int sq = POP_LSB(&bishops);
+    score -= BISHOP_TABLE[flip(sq)];
+  }
+
+  rooks = bbset->rooks[WHITE];
+  while (rooks) {
+    int sq = POP_LSB(&rooks);
+    score += ROOK_TABLE[sq];
+  }
+
+  rooks = bbset->rooks[BLACK];
+  while (rooks) {
+    int sq = POP_LSB(&rooks);
+    score -= ROOK_TABLE[flip(sq)];
+  }
+
+  queens = bbset->queens[WHITE];
+  while (queens) {
+    int sq = POP_LSB(&queens);
+    score += QUEEN_TABLE[sq];
+  }
+
+  queens = bbset->queens[BLACK];
+  while (queens) {
+    int sq = POP_LSB(&queens);
+    score -= QUEEN_TABLE[flip(sq)];
+  }
+
+  kings = bbset->kings[WHITE];
+  while (kings) {
+    int sq = POP_LSB(&kings);
+    score += KING_MIDDLE_GAME_TABLE[sq];
+  }
+
+  kings = bbset->kings[BLACK];
+  while (kings) {
+    int sq = POP_LSB(&kings);
+    score -= KING_MIDDLE_GAME_TABLE[flip(sq)];
+  }
+
+  if (COUNT_BITS(bbset->bishops[WHITE]) >= 2) score += 50;
+  if (COUNT_BITS(bbset->bishops[BLACK]) >= 2) score -= 50;
+
+  return score;
 }
 
 static int calc_mobility_score(const SF_Context *ctx) {
-  (void)(ctx);
-  return 0;
+  SF_Context white_tmpctx   = *ctx;
+  white_tmpctx.search_color = WHITE;
+  MoveList white_moves      = generate_pseudo_legal_moves(&white_tmpctx);
+
+  SF_Context black_tmpctx   = *ctx;
+  black_tmpctx.search_color = BLACK;
+  MoveList black_moves      = generate_pseudo_legal_moves(&black_tmpctx);
+
+  return (white_moves.count - black_moves.count) * 10;
 }
