8d0f795acdb8cde6400b24f0ec8d86564fd7faff
DrShahinstein <1taylanozturk@protonmail.com>
2026-02-20 23:44:20 +0300

    Separate relevant declarations with semantic comment (ui_render.h)

--- include/ui_render.h
+++ include/ui_render.h
@@ -30,5 +30,6 @@ void ui_render_init(SDL_Renderer *renderer);
 void ui_render(SDL_Renderer *renderer, UI_State *ui, EngineWrapper *engine, BoardState *board);
 void ui_render_cleanup(void);
 
+/* -- ui/cache.c  -- */
 void evict_old_text_cache(void);
 SDL_Texture *get_text_cache(TTF_Font *font, const char *text, SDL_Color color, float *out_width, float *out_height);
