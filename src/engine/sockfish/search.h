66b832fdc0128476d317dc672fe12641f236eb65
DrShahinstein <1taylanozturk@protonmail.com>
2026-07-07 21:40:37 +0300

    Declare 'CheckMasks' type (sockfish/search.h)

--- src/engine/sockfish/search.h
+++ src/engine/sockfish/search.h
@@ -2,6 +2,13 @@
 
 #include "sockfish/sockfish.h"
 
+typedef struct {
+  U64 pawn;
+  U64 knight;
+  U64 bishop;
+  U64 rook;
+} CheckMasks;
+
 Move sf_search(const SF_Context *ctx);
 
 int negamax(SF_Context *ctx, unsigned int depth, int ply, int alpha, int beta);
