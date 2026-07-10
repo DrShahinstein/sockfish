7ad2fe8dc592c68dfb8a105eeb7f3b289f42ca81
DrShahinstein <1taylanozturk@protonmail.com>
2026-07-10 13:59:37 +0300

    Initialize killer moves (sockfish.c)

--- src/engine/sockfish/sockfish.c
+++ src/engine/sockfish/sockfish.c
@@ -1,5 +1,6 @@
 #include "sockfish.h"
 #include "evaluation.h"  // sf_init_evaluation()
+#include <string.h>
 
 SF_Context create_sf_ctx(BitboardSet *bitboard_set, Turn search_color, uint8_t castling_rights, Square ep_sq) {
   SF_Context ctx;
@@ -14,6 +15,8 @@ SF_Context create_sf_ctx(BitboardSet *bitboard_set, Turn search_color, uint8_t c
   ctx.hash_key        = 0;
   ctx.history_count   = 0;
   ctx.best            = create_move(0,0);
+
+  memset(ctx.killer_moves, 0, sizeof(ctx.killer_moves));
 
   sf_init_hash_key(&ctx);
   sf_init_evaluation(&ctx);
