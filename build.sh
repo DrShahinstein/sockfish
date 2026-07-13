a6d83b3659cae17c8f277169ac0e111282a986ab
DrShahinstein <1taylanozturk@protonmail.com>
2026-07-13 20:17:45 +0300

    Allow "--release" option to enable release builds (build.sh)

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-meson setup build --prefix ${PWD}/build
+TYPE=debug
+
+if [ "$1" = "--release" ]; then
+  TYPE=release
+fi
+
+meson setup build       \
+  --prefix ${PWD}/build \
+  --buildtype "$TYPE"   \
+
 meson compile -C build
 
