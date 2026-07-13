0f534ac8a4c31e90fe0bb6d5bb24a17d88a33b04
DrShahinstein <1taylanozturk@protonmail.com>
2026-07-13 17:20:29 +0300

    Call init_zobrist_keys() in main.c (src/main.c)

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include "sockfish/transposition_table.h"
 
 int main(int argc, char *argv[]) {
+  init_zobrist_keys();    // init zobrist hashing
   init_attack_tables();   // init precomputed attack tables for sockfish's move generation logic
   init_magic_bitboards(); // init magic bitboards for sliding pieces in move generation logic
   tt_init(32);            // init 32MB transposition table (≈2.000.000 positions)
@@ -21,3 +22,4 @@ int main(int argc, char *argv[]) {
 
   return 0;
 }
+
