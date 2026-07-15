388515e8542a09d9bd4aaed5ce27e9fd2aa36d9c
DrShahinstein <1taylanozturk@protonmail.com>
2026-07-15 16:03:15 +0300

    Declare AppState (include/app.h)

--- include/app.h
+++ include/app.h
@@ -2,12 +2,23 @@
 
 #include "board.h"
 #include "ui.h"
+#include "engine.h"
 
 #define W_WIDTH (BOARD_SIZE + UI_WIDTH)
 #define W_HEIGHT BOARD_SIZE
 #define W_TITLE "Sockfish"
 
-void sdl(void);
+typedef struct {
+  UI_State ui;
+  EngineWrapper engine;
+  BoardState board;
+  U64 last_ini_check;
+  U64 last_ini_mtime;
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+  bool running;
+} AppState;
 
+void sdl(void);
 void uci(void);
 
