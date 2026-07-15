b0cd11c600658badb47ce684b628ea0e780a7f1d
DrShahinstein <1taylanozturk@protonmail.com>
2026-07-15 16:04:47 +0300

    Remove hard-coded transposition table initalization (src/main.c)

--- src/main.c
+++ src/main.c
@@ -5,7 +5,6 @@ int main(int argc, char *argv[]) {
   init_zobrist_keys();    // init zobrist hashing
   init_attack_tables();   // init precomputed attack tables for sockfish's move generation logic
   init_magic_bitboards(); // init magic bitboards for sliding pieces in move generation logic
-  tt_init(32);            // init 32MB transposition table (≈2.000.000 positions)
 
   bool ran_with_uci_param = argc > 1 && strcmp(argv[1], "uci") == 0;
 
