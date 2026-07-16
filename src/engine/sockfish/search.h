86175fe1eed93076f15e2cca9ae988ec9e428a05
DrShahinstein <1taylanozturk@protonmail.com>
2026-07-17 00:52:22 +0300

    Make necessary functions and definitions public (sockfish/search.h)

--- src/engine/sockfish/search.h
+++ src/engine/sockfish/search.h
@@ -1,6 +1,15 @@
 #pragma once
 
 #include "sockfish.h"
+#include "movegen.h"
+
+#define INF 9999999
+#define MATE_SCORE 9000000
+#define MATE_BOUND 8000000
+#define MAX_DEPTH 40
+
+static const int ROOT_PLY=0;
+static const int ALLOW_NULL=true;
 
 typedef struct {
   U64 pawn;
@@ -10,12 +19,14 @@ typedef struct {
 } CheckMasks;
 
 Move sf_search(const SF_Context *ctx);
-
 int negamax(SF_Context *ctx, unsigned int depth, int ply, int alpha, int beta, bool allow_null);
-
 int quiescence_search(SF_Context *ctx, int ply, int alpha, int beta);
-
 int null_move_search(SF_Context *ctx, unsigned int depth, int ply, int beta);
-
 int score_move(const SF_Context *ctx, Move move, Move best_so_far, const CheckMasks *masks, int ply);
+
+bool check_time(SF_Context *ctx);
+void bump_highest_scored_move(int i, MoveList *movelist, int *scores);
+CheckMasks generate_check_masks(const SF_Context *ctx);
+int score_to_tt(int score, int ply);
+int score_from_tt(int score, int ply);
 
