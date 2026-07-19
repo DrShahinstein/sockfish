1c820bc4f1313476f5cee282cf3e8666c99282cd
DrShahinstein <1taylanozturk@protonmail.com>
2026-07-19 21:03:24 +0300

    Create main_sdl.c

---
+++ src/main_sdl.c
@@ -0,0 +1,16 @@
+#include "app.h"
+#include "sockfish/transposition_table.h"
+
+int main(void) {
+  init_zobrist_keys();    // init zobrist hashing
+  init_attack_tables();   // init precomputed attack tables for sockfish's move generation logic
+  init_magic_bitboards(); // init magic bitboards for sliding pieces in move generation logic
+
+  sdl();
+
+  cleanup_magic_bitboards();
+  tt_free();
+
+  return 0;
+}
+
