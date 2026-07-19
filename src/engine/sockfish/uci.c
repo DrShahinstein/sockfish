9f05017a92c5972de47faceef47ae1f439420e3f
DrShahinstein <1taylanozturk@protonmail.com>
2026-07-19 17:59:21 +0300

    Comphrehensively refactor uci_loop and set up async mechanism (sockfish/uci.c)

--- src/engine/sockfish/uci.c
+++ src/engine/sockfish/uci.c
@@ -10,15 +10,39 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <stdatomic.h>
 
 #define IS_TOKEN_END(ch) \
   ((ch) == '\n' || (ch) == '\r' || (ch) == ' ' || (ch) == '\t' || (ch) == '\0')
 
-static const char *START_FEN="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+static const char *START_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
-static Move uci_parse_move(SF_Context *ctx, const char *move_str);
+/* Internal Helpers */
 static void uci_parse_fen(const char *fen, SF_Context *ctx);
+static Move uci_parse_move(SF_Context *ctx, const char *move_str);
 static void print_best(Move best);
+static void parse_go(const char *line, SF_Context *ctx);
+
+/* Command Handlers */
+static void handle_uci(const SF_Config *cfg);
+static void handle_setoption(const char *line, SF_Config *cfg);
+static void handle_ucinewgame(SF_Context *ctx);
+static void handle_position(const char *line, SF_Context *ctx);
+static void handle_go(const char *line, const SF_Context *uci_ctx, const SF_Config *cfg);
+
+/* Async Logic */
+static void *async_search_thread_main(void *arg);
+static void async_search_reap(void);
+static void async_search_start(const SF_Context *base_ctx, int threads);
+static void async_search_request_stop(void);
+static void async_search_shutdown(void);
+
+static AsyncSearch async_search = {
+  .thread_valid = false,
+  .running      = false,
+  .stop_flag    = false,
+};
 
 static inline void init_uci_config(SF_Config *cfg) {
   /* Default values for options */
@@ -28,8 +52,7 @@ static inline void init_uci_config(SF_Config *cfg) {
 
 static void apply_default_options(SF_Context *ctx, SF_Config *uci_cfg) {
   /* option name Hash */
-  int megabytes = uci_cfg->tt_size_mb;
-  tt_init(megabytes);
+  tt_init(uci_cfg->tt_size_mb);
 
   /* option name Threads */
   ctx->threads = uci_cfg->threads;
@@ -54,185 +77,165 @@ void uci_loop(void) {
 
   while (fgets(line, sizeof(line), stdin)) {
     if (strncmp(line, "uci", 3) == 0 && IS_TOKEN_END(line[3])) {
-      printf("id name Sockfish\n");
-      printf("id author DrShahinstein\n");
-      printf("option name Hash type spin default %d min 1 max 1024\n",   uci_config.tt_size_mb);
-      printf("option name Threads type spin default %d min 1 max 128\n", uci_config.threads);
-      printf("uciok\n");
+      handle_uci(&uci_config);
     }
-    
-    else if (strncmp(line, "isready", 7) == 0) {
+    else if (strncmp(line, "isready", 7) == 0 && IS_TOKEN_END(line[7])) {
       printf("readyok\n");
     }
-
-    else if (strncmp(line, "setoption", 9) == 0) {
-      char *name_ptr = strstr(line, "name ");
-      char *val_ptr  = strstr(line, "value ");
-      
-      if (name_ptr && val_ptr) {
-        name_ptr += 5;
-        val_ptr  += 6;
-        
-        if (strncmp(name_ptr, "Hash", 4) == 0) {
-          int new_hash = atoi(val_ptr);
-          if (new_hash > 0 && new_hash != uci_config.tt_size_mb) {
-            uci_config.tt_size_mb = new_hash;
-            tt_free();
-            tt_init(uci_config.tt_size_mb);
-          }
-        }
-
-        else if (strncmp(name_ptr, "Threads", 7) == 0) {
-          int new_threads = atoi(val_ptr);
-          if (new_threads > 0) {
-            uci_config.threads = new_threads;
-          }
-        }
-      }
+    else if (strncmp(line, "setoption", 9) == 0 && IS_TOKEN_END(line[9])) {
+      handle_setoption(line, &uci_config);
     }
-    
-    else if (strncmp(line, "ucinewgame", 10) == 0) {
-      tt_clear();
-      uci_ctx.history_count = 0;
-      memset(uci_ctx.killer_moves, 0, sizeof(uci_ctx.killer_moves));
+    else if (strncmp(line, "ucinewgame", 10) == 0 && IS_TOKEN_END(line[10])) {
+      handle_ucinewgame(&uci_ctx);
     }
-    
-    else if (strncmp(line, "position", 8) == 0) {
-      if (strstr(line, "startpos")) {
-        uci_parse_fen(START_FEN, &uci_ctx);
-      }
-
-      else if (strstr(line, "fen ")) {
-        char *fen_ptr = strstr(line, "fen ") + 4;
-        uci_parse_fen(fen_ptr, &uci_ctx);
-      }
-      
-      char *moves_ptr = strstr(line, "moves ");
-      if (moves_ptr != NULL) {
-        moves_ptr += 6; 
-        char *move_str = strtok(moves_ptr, " \n");
-        
-        while (move_str != NULL) {
-          Move m = uci_parse_move(&uci_ctx, move_str);
-          if (m != 0) {
-            MoveHistory hist;
-            make_move(&uci_ctx, m, &hist);
-          }
-          move_str = strtok(NULL, " \n");
-        }
-      }
+    else if (strncmp(line, "position", 8) == 0 && IS_TOKEN_END(line[8])) {
+      handle_position(line, &uci_ctx);
     }
-    
-    else if (strncmp(line, "go", 2) == 0) {
-      int wtime=0, btime=0, winc=0, binc=0, movetime=0;
-
-      uci_ctx.time_limit  = 0;
-      uci_ctx.depth_limit = 0;
-      uci_ctx.nodes_limit = 0;
-      uci_ctx.infinite    = false;
-
-      if (strstr(line, "infinite")) {
-        uci_ctx.infinite = true;
-      } 
-
-      if (strstr(line, "depth")) {
-        sscanf(strstr(line, "depth") + 6, "%d", &uci_ctx.depth_limit);
-      } 
-
-      if (strstr(line, "nodes")) {
-        sscanf(strstr(line, "nodes") + 6, "%llu",
-            (long long unsigned int*) &uci_ctx.nodes_limit);
-      } 
-
-      if (strstr(line, "movetime")) {
-        sscanf(strstr(line, "movetime") + 9, "%d", &movetime);
-        uci_ctx.time_limit = movetime;
-      }
-
-      else if (!uci_ctx.infinite && !uci_ctx.depth_limit && !uci_ctx.nodes_limit) {
-        if (strstr(line, "wtime")) sscanf(strstr(line, "wtime") + 5, "%d", &wtime);
-        if (strstr(line, "btime")) sscanf(strstr(line, "btime") + 5, "%d", &btime);
-        if (strstr(line, "winc"))  sscanf(strstr(line, "winc")  + 5, "%d", &winc);
-        if (strstr(line, "binc"))  sscanf(strstr(line, "binc")  + 5, "%d", &binc);
-
-        int time_left = (uci_ctx.search_color == WHITE) ? wtime : btime;
-        int inc       = (uci_ctx.search_color == WHITE) ? winc  : binc;
-
-        uci_ctx.time_limit = (time_left / 30) + (inc / 2);
-
-        if (uci_ctx.time_limit < 50)
-          uci_ctx.time_limit = 50;
-      }
-
-      uci_ctx.threads = uci_config.threads;
-
-      Move best = sf_search(&uci_ctx);
-
-      print_best(best);
+    else if (strncmp(line, "go", 2) == 0 && IS_TOKEN_END(line[2])) {
+      handle_go(line, &uci_ctx, &uci_config);
     }
-
-    else if (strncmp(line, "d", 1) == 0 && line[1] == '\n') {
+    else if (strncmp(line, "d", 1) == 0 && IS_TOKEN_END(line[1])) {
       print_bitboard(uci_ctx.bitboard_set.occupied);
     }
-
-    else if (strncmp(line, "stop", 4) == 0) {
-      if (uci_ctx.should_stop) *uci_ctx.should_stop=true;
+    else if (strncmp(line, "stop", 4) == 0 && IS_TOKEN_END(line[4])) {
+      async_search_request_stop();
     }
-    
-    else if (strncmp(line, "quit", 4) == 0) {
+    else if (strncmp(line, "quit", 4) == 0 && IS_TOKEN_END(line[4])) {
       break;
     }
   }
+
+  async_search_shutdown();
+}
+
+/* ==================== Command Handlers ==================== */
+
+static void handle_uci(const SF_Config *cfg) {
+  printf("id name Sockfish\n");
+  printf("id author DrShahinstein\n");
+  printf("option name Hash type spin default %d min 1 max 1024\n",   cfg->tt_size_mb);
+  printf("option name Threads type spin default %d min 1 max 128\n", cfg->threads);
+  printf("uciok\n");
+}
+
+static void handle_setoption(const char *line, SF_Config *cfg) {
+  char *name_ptr = strstr(line, "name ");
+  char *val_ptr  = strstr(line, "value ");
+
+  if (!name_ptr || !val_ptr)
+    return;
+
+  name_ptr += 5;
+  val_ptr  += 6;
+
+  if (strncmp(name_ptr, "Hash", 4) == 0) {
+    int new_hash = atoi(val_ptr);
+    if (new_hash > 0 && new_hash != cfg->tt_size_mb) {
+      cfg->tt_size_mb = new_hash;
+      tt_free();
+      tt_init(cfg->tt_size_mb);
+    }
+  }
+  else if (strncmp(name_ptr, "Threads", 7) == 0) {
+    int new_threads = atoi(val_ptr);
+    if (new_threads > 0)
+      cfg->threads = new_threads;
+  }
+}
+
+static void handle_ucinewgame(SF_Context *ctx) {
+  tt_clear();
+  ctx->history_count = 0;
+  memset(ctx->killer_moves, 0, sizeof(ctx->killer_moves));
+}
+
+static void handle_position(const char *line, SF_Context *ctx) {
+  if (strstr(line, "startpos")) {
+    uci_parse_fen(START_FEN, ctx);
+  } else if (strstr(line, "fen ")) {
+    uci_parse_fen(strstr(line, "fen ") + 4, ctx);
+  }
+
+  char *moves_ptr = strstr(line, "moves ");
+  if (!moves_ptr)
+    return;
+
+  moves_ptr += 6;
+  char *move_str = strtok(moves_ptr, " \n");
+
+  while (move_str != NULL) {
+    Move m = uci_parse_move(ctx, move_str);
+    if (m != 0) {
+      MoveHistory hist;
+      make_move(ctx, m, &hist);
+    }
+    move_str = strtok(NULL, " \n");
+  }
+}
+
+static void handle_go(const char *line, const SF_Context *uci_ctx, const SF_Config *cfg) {
+  SF_Context go_ctx = *uci_ctx;
+  parse_go(line, &go_ctx);
+  async_search_start(&go_ctx, cfg->threads);
+}
+
+static void parse_go(const char *line, SF_Context *ctx) {
+  int wtime=0, btime=0, winc=0, binc=0, movetime=0;
+
+  ctx->time_limit  = 0;
+  ctx->depth_limit = 0;
+  ctx->nodes_limit = 0;
+  ctx->infinite    = false;
+
+  if (strstr(line, "infinite"))
+    ctx->infinite = true;
+
+  if (strstr(line, "depth"))
+    sscanf(strstr(line, "depth") + 6, "%d", &ctx->depth_limit);
+
+  if (strstr(line, "nodes"))
+    sscanf(strstr(line, "nodes") + 6, "%llu", (unsigned long long *)&ctx->nodes_limit);
+
+  if (strstr(line, "movetime")) {
+    sscanf(strstr(line, "movetime") + 9, "%d", &movetime);
+    ctx->time_limit = movetime;
+    return;
+  }
+
+  if (ctx->infinite || ctx->depth_limit || ctx->nodes_limit)
+    return;
+
+  if (strstr(line, "wtime")) sscanf(strstr(line, "wtime") + 5, "%d", &wtime);
+  if (strstr(line, "btime")) sscanf(strstr(line, "btime") + 5, "%d", &btime);
+  if (strstr(line, "winc"))  sscanf(strstr(line, "winc")  + 5, "%d", &winc);
+  if (strstr(line, "binc"))  sscanf(strstr(line, "binc")  + 5, "%d", &binc);
+
+  int time_left = (ctx->search_color == WHITE) ? wtime : btime;
+  int inc       = (ctx->search_color == WHITE) ? winc  : binc;
+
+  ctx->time_limit = (time_left / 30) + (inc / 2);
+  if (ctx->time_limit < 50)
+    ctx->time_limit = 50;
 }
 
 static void print_best(Move best) {
-  char from_alg[3], to_alg[3];
-
-  sq_to_alg(move_from(best), from_alg);
-  sq_to_alg(move_to(best),   to_alg);
-
-  if (move_type(best) == MOVE_PROMOTION) {
-    char promo = 'q';
-    switch (move_promotion(best)) {
-      case PROMOTE_ROOK:   promo = 'r'; break;
-      case PROMOTE_BISHOP: promo = 'b'; break;
-      case PROMOTE_KNIGHT: promo = 'n'; break;
-      default:             promo = 'q'; break;
-    }
-    printf("bestmove %s%s%c\n", from_alg, to_alg, promo);
-  } else {
-    printf("bestmove %s%s\n", from_alg, to_alg);
-  }
+  char buf[6];
+  move_to_uci_string(best, buf);
+  printf("bestmove %s\n", buf);
 }
 
 static Move uci_parse_move(SF_Context *ctx, const char *move_str) {
   MoveList list = sf_generate_moves(ctx);
-  
+
   for (int i = 0; i < list.count; ++i) {
     Move m = list.moves[i];
     char buf[6];
-    
-    char from_alg[3], to_alg[3];
-    sq_to_alg(move_from(m), from_alg);
-    sq_to_alg(move_to(m), to_alg);
-    
-    if (move_type(m) == MOVE_PROMOTION) {
-      char promo = 'q';
-      switch (move_promotion(m)) {
-        case PROMOTE_ROOK:   promo = 'r'; break;
-        case PROMOTE_BISHOP: promo = 'b'; break;
-        case PROMOTE_KNIGHT: promo = 'n'; break;
-        default:             promo = 'q'; break;
-      }
-      snprintf(buf, sizeof(buf), "%s%s%c", from_alg, to_alg, promo);
-    } else {
-      snprintf(buf, sizeof(buf), "%s%s", from_alg, to_alg);
-    }
-    
-    if (strncmp(move_str, buf, strlen(buf)) == 0) {
+    move_to_uci_string(m, buf);
+
+    if (strncmp(move_str, buf, strlen(buf)) == 0)
       return m;
-    }
   }
+
   return 0;
 }
 
@@ -287,8 +290,55 @@ static void uci_parse_fen(const char *fen, SF_Context *ctx) {
   ctx->enpassant_sq    = ep_sq;
   ctx->halfmove_clock  = 0;
   ctx->history_count   = 0;
-  
+
   sf_init_hash_key(ctx);
   sf_init_evaluation(ctx);
+}
+
+/* ==================== Async Logic ==================== */
+
+static void *async_search_thread_main(void *arg) {
+  (void)arg;
+
+  Move best = sf_search(&async_search.ctx);
+  print_best(best);
+  atomic_store(&async_search.running, false);
+
+  return NULL;
+}
+
+static void async_search_reap(void) {
+  if (async_search.thread_valid && !atomic_load(&async_search.running)) {
+    pthread_join(async_search.thread, NULL);
+    async_search.thread_valid = false;
+  }
+}
+
+static void async_search_start(const SF_Context *base_ctx, int threads) {
+  if (atomic_load(&async_search.running))
+    return;
+
+  async_search_reap();
+
+  async_search.ctx             = *base_ctx;
+  async_search.ctx.threads     = threads;
+  async_search.stop_flag       = false;
+  async_search.ctx.should_stop = &async_search.stop_flag;
+
+  atomic_store(&async_search.running, true);
+  pthread_create(&async_search.thread, NULL, async_search_thread_main, NULL);
+  async_search.thread_valid = true;
+}
+
+static void async_search_request_stop(void) {
+  async_search.stop_flag = true;
+}
+
+static void async_search_shutdown(void) {
+  async_search_request_stop();
+  if (async_search.thread_valid) {
+    pthread_join(async_search.thread, NULL);
+    async_search.thread_valid = false;
+  }
 }
 
