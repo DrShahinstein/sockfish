d6eafbcc17c88a5295a82e91861f04129a901776
DrShahinstein <1taylanozturk@protonmail.com>
2026-07-19 17:57:14 +0300

    Declare AsyncSearch structure (sockfish/uci.h)

--- src/engine/sockfish/uci.h
+++ src/engine/sockfish/uci.h
@@ -1,4 +1,16 @@
 #pragma once
 
+#include "sockfish.h"
+#include <pthread.h>
+#include <stdatomic.h>
+
+typedef struct {
+  pthread_t thread;
+  bool thread_valid;
+  atomic_bool running;
+  volatile bool stop_flag;
+  SF_Context ctx;
+} AsyncSearch;
+
 void uci_loop(void);
 
