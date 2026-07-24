11b76d2eaf67c9832963fa091da1f7bb1dae05e3
DrShahinstein <1taylanozturk@protonmail.com>
2026-07-25 00:19:16 +0300

    Redefine TT entry layout for 64-bit atomic signature and payload packing (sockfish/transposition_table.h)

--- src/engine/sockfish/transposition_table.h
+++ src/engine/sockfish/transposition_table.h
@@ -1,6 +1,24 @@
 #pragma once
 
 #include "sockfish.h"
+#include <stdatomic.h>
+
+/*
+ * 64-bit TT Payload Bit Layout:
+ *
+ *  [62]       [57..56]    [55..48]    [47..32]    [31..0]
+ *   |            |           |           |           |
+ *   |            |           |           |           +-- Score (32 bit, int32_t)
+ *   |            |           |           +-------------- Best Move (16 bit, Move)
+ *   |            |           +-------------------------- Depth (8 bit, uint8_t)
+ *   |            +-------------------------------------- Flag (2 bit, TT_Flag)
+ *   +--------------------------------------------------- Occupied Flag (1 bit)
+ */
+#define TT_SCORE_SHIFT     0
+#define TT_MOVE_SHIFT     32
+#define TT_DEPTH_SHIFT    48
+#define TT_FLAG_SHIFT     56
+#define TT_OCCUPIED_MASK  (UINT64_C(1) << 62)
 
 typedef enum {
   TT_EXACT,
@@ -9,12 +27,10 @@ typedef enum {
 } TT_Flag;
 
 typedef struct {
-  U64 hash_key;
-  int score;
-  Move best_move;
-  uint8_t depth;
-  uint8_t flag;
+  _Atomic U64 signature;
+  _Atomic U64 payload;
 } TT_Entry;
+_Static_assert(sizeof(TT_Entry) == 16, "TT_Entry must be 16 bytes");
 
 extern TT_Entry *tt_table;
 extern int tt_num_entries;
